64b399e7807ea1215ee3e3e1998c7dd22752a588
Alex <cnalex_2@hotmail.com>
2022-08-10 21:34:11 -0500

    perfil

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Contenedor from './componentes/contenedor/Contenedor';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Nav from './componentes/nav/Nav';
+import PromiseContainer from './componentes/promiseContainer/PromiseContainer';
+import Publicidad from './componentes/publicidad/Publicidad';
+
 
 function App() {
   return (
-   <Contenedor/>
+    <BrowserRouter> 
+      <Publicidad/>
+      <Nav/>
+      <Routes>
+        {/* <Route path='/home' element={<Fecht/>} /> */}
+        {/* <Route path='/' element={<Fecht/>} /> */}
+        <Route path='/promesa' element={<PromiseContainer/>}/>
+        {/* <Route path='/filtro/:category' element={<Fecht/>}/> /     */}
+        {/* <Route path='/detalle/:id' element={<DetalleContainer/>} /> */}
+        {/* <Route path='/filtroinput' element={<Buscador/>}/> */}
+        {/* <Route path='/error' element={<Error/>} /> */}
+        {/* <Route path='*' element={<Navigate to='/error'/>}/> */}
+      </Routes>
+    </BrowserRouter> 
   );
 }
 
-export default App;
+export default App
---
+++ src/componentes/publicidad/Publicidad.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Publicidad = () => {
+  return (
+    <div style={{background: " #dae66f ", textAlign:"center", minHeight:'8vh'}}>
+        <h2 style={{color:'white'}}>Redes sociales en Publicidad</h2>
+    </div>
+  )
+}
+
+export default Publicidad
